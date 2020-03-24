2779d32563230ae82ff5fa565dfccf108ca4503f
Felipe Araujo Camelo <48761994+ViktorReznovCOD@users.noreply.github.com>
2020-03-24 12:39:19 -0300

    First
    Start do projeto, com todas as configurações iniciais necessárias.
    Segue tbm um arquivo 'Readme' para auxílio

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\viktor reznov\documents\github\mysite_scmp\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
