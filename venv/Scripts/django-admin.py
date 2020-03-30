62c31ee84a2772950eecdc2297cc486bba826ede
Osvaldo Santana Neto <github@osantana.me>
2020-03-30 12:08:19 +0200

    Remove venv directory that needs to be created only locally

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\viktor reznov\documents\github\mysite_scmp\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
